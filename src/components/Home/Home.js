a6c20779885d08ed84b446e1c42c4b86729261de
samoudud <moududpro@gmail.com>
2022-01-09 03:17:06 +0600

    first view done

---
+++ src/components/Home/Home.js
@@ -0,0 +1,67 @@
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import Section from '../Section/Section';
+
+const Home = () => {
+    const [carInfo, setCarInfo] = useState([]);
+    useEffect(() => {
+        fetch('/carInfo.json')
+            .then(res => res.json())
+            .then(data => setCarInfo(data))
+    }, [])
+    return (
+        <Container>
+            {/* {
+                carInfo.map(info => <Section key={info.id} description={info.description} backgroundImg={info.image} leftBtnText="Custom Order"
+                rightBtnText="Existing Inventory"></Section>)
+            } */}
+            <Section
+                title="Model S"
+                description="Order Online for Touchless Delivery"
+                backgroundImg="model-s.jpg"
+                leftBtnText="Custom Order"
+                rightBtnText="Existing Inventory"
+            />
+            <Section
+                title="Model 3"
+                description="Order Online for Touchless Delivery"
+                backgroundImg="model-3.jpg"
+                leftBtnText="Custom Order"
+                rightBtnText="Existing Inventory"
+            />
+            <Section
+                title="Model X"
+                description="Order Online for Touchless Delivery"
+                backgroundImg="model-x.jpg"
+                leftBtnText="Custom Order"
+                rightBtnText="Existing Inventory"
+            />
+            <Section
+                title="Model Y"
+                description="Order Online for Touchless Delivery"
+                backgroundImg="model-y.jpg"
+                leftBtnText="Custom Order"
+                rightBtnText="Existing Inventory"
+            />
+            <Section
+                title="Lowest Cost Solar Panels in America"
+                description="Money-back guarantee"
+                backgroundImg="solar-panel.jpg"
+                leftBtnText="Order now"
+                rightBtnText="Learn more"
+            />
+            <Section
+                title="Accessories"
+                description=""
+                backgroundImg="accessories.jpg"
+                leftBtnText="Order now"
+            />
+        </Container>
+    );
+};
+
+export default Home;
+
+const Container = styled.div`
+    height: 100vh;
+`
